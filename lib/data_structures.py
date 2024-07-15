853d49f810f120a261e6968e5afc71e8c9edc3ae
yuxi Shi <ys3599@tc.columbia.edu>
2024-07-15 15:17:36 -0400

    complete lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,55 @@ spicy_foods = [
     },
 ]
 
-def get_names(spicy_foods):
-    pass
+def get_names(spicy_foods:list):
+    return [food['name'] for food in spicy_foods]
+print(get_names(spicy_foods))
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+print(get_spiciest_foods(spicy_foods))
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        peppers = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {peppers}")
+print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+get_spicy_food_by_cuisine(spicy_foods, "American")
+get_spicy_food_by_cuisine(spicy_foods, "Thai")
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            name = food["name"]
+            cuisine = food["cuisine"]
+            heat_level = food["heat_level"]
+            peppers = "🌶" * heat_level
+            print(f"{name} ({cuisine}) | Heat Level: {peppers}")
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    if not spicy_foods: 
+        return 0  
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    return round(total_heat / len(spicy_foods))
+get_average_heat_level(spicy_foods)
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+def create_spicy_food(spicy_foods, new_spicy_food):
+    spicy_foods.append(new_spicy_food)
+    return spicy_foods
+new_food = {
+    'name': 'Griot',
+    'cuisine': 'Haitian',
+    'heat_level': 10,
+}
+
+updated_spicy_foods = create_spicy_food(spicy_foods, new_food)
+print(updated_spicy_foods)
